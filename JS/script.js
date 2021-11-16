0355dccbde8e22f268e7dbbc561376f7b7410e0d
ReichiSama <mertarass@hotmail.com>
2021-11-16 17:06:21 +0300

    deneme

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+//console.log("External");
+
+//let selam = "Hello";
+//console.log(selam);
+
+//var deger = "Hello";
+//console.log(deger);
+//var deger = "Merhaba";
+//console.log(deger);
+
+//function fonn(){
+    //var deger = "text";
+//}
+
+//fonn();
+//console.log(deger);
+
+const text = "Javascript";
+
+
+
